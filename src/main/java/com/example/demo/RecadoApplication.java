b97bc0ce523cb86e835b7c2c71b997ee48b1d9a8
Antonio Salinas García (vedoque.com) <73043287+antoniocesur@users.noreply.github.com>
2022-12-26 21:46:15 +0100

    Ya se cuentan los "me gusta"

--- src/main/java/com/example/demo/RecadoApplication.java
+++ src/main/java/com/example/demo/RecadoApplication.java
@@ -17,6 +17,7 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Locale;
 
+//TODO: Retweet, "me gusta" AJAX sin recargar,Login y registro
 @SpringBootApplication
 public class RecadoApplication {
 	@Autowired
@@ -25,9 +26,9 @@ public class RecadoApplication {
 	ServicioRecado servicioRecado;
 
 	public static void main(String[] args) {
-		//Esto lo que hace es iniciar MySQL al arrancar la app
+		//Esto inicia MySQL al arrancar la app
 		//Como es lógico, solo funciona si tienes instalado MySQL en la misma ruta
-		//Si no la conoces e inicias MySQL desde el terminal o con Xampp(o similares) puedes borrar estas líneas
+		//Si no la conoces o prefieres iniciar MySQL desde el terminal o con Xampp(o similares) puedes borrar estas líneas
 		String command = "C:\\xampp\\mysql\\bin\\mysqld.exe";
 		try{
 			Process process = Runtime.getRuntime().exec(command);
@@ -40,21 +41,17 @@ public class RecadoApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(){
 		return args -> {
-			//Generamos datos de relleno en la base de datos con Faker y Pravatar (nos da un avatar para email)
+			//Generamos datos de relleno en la base de datos con Faker y Pravatar (nos da un avatar diferente para cada email)
 			Autor admin=new Autor("admin", "asalinasci@gmail.com", "1234", "");
 			admin.setAvatar("https://i.pravatar.cc/150?u=" + admin.getEmail());
 			servicioAutor.save(admin);
 			int max = 10;
 			Faker faker = new Faker(new Locale("es-ES"));
-			HashMap<Integer, Autor> mapAutores = new HashMap<>();
-			HashMap<Integer, Recado> mapRecados = new HashMap<>();
 
 			for(int i = 0; i < max; i++){
 				Autor autor = new Autor(faker.name().firstName(), faker.internet().emailAddress(), faker.internet().password(),"https://i.pravatar.cc/150?u=");
 				autor.setAvatar(autor.getAvatar() + autor.getEmail());
 				Recado recado=new Recado(faker.chuckNorris().fact(), autor,Date.valueOf(LocalDate.now()));
-				mapAutores.put(i, autor);
-				mapRecados.put(i, recado);
 				servicioAutor.save(autor);
 				servicioRecado.save(recado);
 			}
